661af60c7b84d728d65797aa777e65e6c5790124
danleyb2 <ndieksman@gmail.com>
2016-12-29 07:54:08 +0300

    add register and login ability, add star and unstar endpoints for Company on web

--- ussdke/urls.py
+++ ussdke/urls.py
@@ -1,89 +1,93 @@
-from django.conf.urls import url,include
-from rest_framework import routers
-from . import views
-
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from . import views
 
-#router = routers.DefaultRouter()
-#router.register(r'ussds', views.UssdViewSet)
-#router.register(r'ussds/suggest', views.UssdSuggestViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'ussds', views.UssdViewSet)
+# router.register(r'ussds/suggest', views.UssdSuggestViewSet)
 
-#router.register(r'companies', views.CompanyList.as_view())
-#router.register(r'companies/suggest', views.CompanyViewSet)
+# router.register(r'companies', views.CompanyList.as_view())
+# router.register(r'companies/suggest', views.CompanyViewSet)
 
 
 
 urlpatterns = format_suffix_patterns([
     url(r'^$', views.home, name='index'),
+    url(r'^signin/$', views.LoginView.as_view(), name='login'),
+    url(r'^signup/$', views.RegisterView.as_view(), name='register'),
+    url(r'^signout/$', views.LogoutView.as_view(), name='logout'),
+
     url(r'^about$', views.about, name='about'),
 
-    #url(r'^api2/',include(router.urls)),
+    # url(r'^api2/',include(router.urls)),
     url(r'^ussds/', include(
         [
-            url(r'^$', views.UssdViewSet.as_view(),name='info'),
-            url(r'^suggest', views.UssdSuggest.as_view(),name='suggest'),
+            url(r'^$', views.UssdViewSet.as_view(), name='info'),
+            url(r'^suggest', views.UssdSuggest.as_view(), name='suggest'),
             # url(r'^api/ussds',views.companies),
             # url(r'^invalidate',views.companies),
             # url(r'^fav',views.companies),
             url(r'^(?P<pk>[0-9]+)/', include([
                 url(r'^$', views.UssdDetailViewSet.as_view(), name='instance'),
-                #url(r'invalidate', views.UssdDetailInvalidate.as_view(), name='invalidate'), #todo
+                # url(r'invalidate', views.UssdDetailInvalidate.as_view(), name='invalidate'), #todo
 
             ], namespace='ussd')),
-        ]),name='ussds'
+        ]), name='ussds'
         ),
 
     url(r'^companies/', include(
         [
-            url(r'^$', views.CompanyViewSet.as_view(),name='info'),
-            url(r'^suggest', views.CompanySuggest.as_view(),name='suggest'),
+            url(r'^$', views.CompanyViewSet.as_view(), name='info'),
+            url(r'^suggest', views.CompanySuggest.as_view(), name='suggest'),
             # url(r'^ussds',views.ussds),
             url(r'^(?P<pk>[0-9]+)/', include([
-                url(r'^$', views.CompanyDetail.as_view(),name='instance'),
-                url(r'ussds', views.CompanyUssdViewSet.as_view(),name='ussds')
-            ],namespace='company'),),
-        ],namespace='companies'),
+                url(r'^$', views.CompanyDetail.as_view(), name='instance'),
+                url(r'^ussds/', views.CompanyUssdViewSet.as_view(), name='ussds'),
+                url(r'^star/', views.star_company, name='star'),
+                url(r'^unstar/', views.un_star_post, name='unstar'),
+            ], namespace='company'), ),
+        ], namespace='companies'),
         ),
 
-    url(r'^api/',include([
+    url(r'^api/', include([
 
-        url(r'^ussds/',include(
+        url(r'^ussds/', include(
             [
-                url(r'^$',views.UssdViewSet.as_view(),name='info'),
-                url(r'^suggest',views.UssdSuggest.as_view(),name='suggest'),
-                #url(r'^api/ussds',views.companies),
-                #url(r'^invalidate',views.companies),
-                #url(r'^fav',views.companies),
-                url(r'^(?P<pk>[0-9]+)/',include([
-                    url(r'^$',views.UssdDetailViewSet.as_view(), name = 'instance'),
-                    url(r'invalidation/',include([
-                        url(r'invalidate',views.UssdDetailInvalidate.as_view(), name = 'invalidate'),
-                        url(r'list',views.UssdDetailInvalidationList.as_view(), name = 'list'),
-                    ],namespace='invalidation'))
-
-                ],namespace='ussd')),
-            ],namespace='ussds'),
-            ),
-
-        url(r'^companies/',include(
-            [
-                url(r'^$',views.CompanyViewSet.as_view(),name='info'),
-                url(r'^suggest',views.CompanySuggest.as_view(),name='suggest'),
-                #url(r'^ussds',views.ussds),
+                url(r'^$', views.UssdViewSet.as_view(), name='info'),
+                url(r'^suggest', views.UssdSuggest.as_view(), name='suggest'),
+                # url(r'^api/ussds',views.companies),
+                # url(r'^invalidate',views.companies),
+                # url(r'^fav',views.companies),
                 url(r'^(?P<pk>[0-9]+)/', include([
-                    url(r'^$', views.CompanyDetailViewSet.as_view(),name='instance'), #todo
-                    url(r'ussds',views.CompanyUSSDDetail.as_view(),name='ussds') #todo
-                ],namespace='company')),
-            ],namespace='companies')
+                    url(r'^$', views.UssdDetailViewSet.as_view(), name='instance'),
+                    url(r'invalidation/', include([
+                        url(r'invalidate', views.UssdDetailInvalidate.as_view(), name='invalidate'),
+                        url(r'list', views.UssdDetailInvalidationList.as_view(), name='list'),
+                    ], namespace='invalidation'))
+
+                ], namespace='ussd')),
+            ], namespace='ussds'),
             ),
 
+        url(r'^companies/', include(
+            [
+                url(r'^$', views.CompanyViewSet.as_view(), name='info'),
+                url(r'^suggest', views.CompanySuggest.as_view(), name='suggest'),
+                # url(r'^ussds',views.ussds),
+                url(r'^(?P<pk>[0-9]+)/', include([
+                    url(r'^$', views.CompanyDetailViewSet.as_view(), name='instance'),  # todo
+                    url(r'ussds', views.CompanyUSSDDetail.as_view(), name='ussds')  # todo
+                ], namespace='company')),
+            ], namespace='companies')
+            ),
 
-        ],namespace='api'))
+    ], namespace='api'))
 
 ])
 
 from rest_framework.authtoken import views
+
 urlpatterns += [
     url(r'^api-token-auth/', views.obtain_auth_token)
 ]
--- ussdke/views.py
+++ ussdke/views.py
@@ -1,19 +1,31 @@
-import re
+import json
 
+import re,logging
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render, render_to_response
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.utils.http import is_safe_url
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import FormView, RedirectView
 from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.renderers import TemplateHTMLRenderer,JSONRenderer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView
 
-from ussdke.models import USSD, Company, Code, Invalidation
-from ussdke.serializers import UssdSerializer, CompanySerializer, CodeSerializer, CompanyUssdSerializer, InvalidationSerializer
+from ussdke.models import *
+from ussdke.forms import *
+from ussdke.serializers import UssdSerializer, CompanySerializer, CompanyUssdSerializer, InvalidationSerializer
+
+log = logging.getLogger('ussdke')
 
 def about(request):
     context = {
@@ -26,7 +38,7 @@ def home(request):
     #context={'companies': CompanySerializer(companies,many=True).data}
     context={'companies': Company.objects.all(),'ussds':USSD.objects.all()[:5]}
     #print context
-    return render(request, 'ussdke/index.html', context)
+    return render(request, 'ussdke/main.html', context)
 
 
 def companies(request):
@@ -386,3 +398,156 @@ class CompanyUSSDDetailOld(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class LoginView(FormView):
+    """
+    Provides the ability to login as a user with a username and password
+    """
+    template_name = 'ussdke/login.html'
+    success_url = '/'
+    form_class = AuthenticationForm
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    @method_decorator(sensitive_post_parameters('password'))
+    @method_decorator(csrf_protect)
+    @method_decorator(ensure_csrf_cookie)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        log.info("Dispatch")
+
+        # Sets a test cookie to make sure the user has cookies enabled
+        # request.session.set_test_cookie()
+        # log.info("Test Cookie Set")
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        log.info('Next: auth_login')
+        auth_login(self.request, form.get_user())
+        log.info('After: auth_login')
+
+        # If the test cookie worked, go ahead and
+        # delete it since its no longer needed
+        # if self.request.session.test_cookie_worked():
+        #    log.info('test cookie worked')
+        #    self.request.session.delete_test_cookie()
+
+        return super(LoginView, self).form_valid(form)
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get(self.redirect_field_name)
+        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
+            redirect_to = self.success_url
+        return redirect_to
+
+class LogoutView(RedirectView):
+    """
+    Provides users the ability to logout
+    """
+    template_name = 'web/logout.html'
+    url = '/'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+class RegisterView(FormView):
+    """
+    Provides the ability to register as a user with a username and password
+    """
+    template_name = 'ussdke/register.html'
+    success_url = '/'
+    form_class = UserCreateForm
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    @method_decorator(sensitive_post_parameters('password1', 'password2'))
+    @method_decorator(csrf_protect)
+    @method_decorator(ensure_csrf_cookie)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        # Sets a test cookie to make sure the user has cookies enabled
+        request.session.set_test_cookie()
+
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+
+        new_user = form.save()
+        # user.set_password(user.password)
+        # user.save()
+
+        user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password1']
+                            )
+        login(self.request, user)
+
+        # If the test cookie worked, go ahead and
+        # delete it since its no longer needed
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+
+        return super(RegisterView, self).form_valid(form)
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get(self.redirect_field_name)
+        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
+            redirect_to = self.success_url
+        return redirect_to
+
+def star_company(request, pk):
+    if request.method == 'POST':
+        response_data = {}
+
+        company = Company.objects.get(pk=pk)
+        author = request.user
+
+        star, created = CompanyStar.objects.get_or_create(author=author, company=company)
+
+        response_data['result'] = 'Company Star successful!'
+        response_data['company'] = company.pk
+
+        response_data['next'] = "UnStar"
+        response_data['next_url'] = "/companies/" + str(company.pk) + "/unstar/"
+
+        response_data['count'] = company.stars.count()
+        response_data['created'] = star.created_date.strftime('%B %d, %Y %I:%M %p')
+        response_data['author'] = star.author.username
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
+
+def un_star_post(request, pk):
+    if request.method == 'POST':
+        response_data = {}
+
+        company = Company.objects.get(pk=pk)
+        author = request.user
+
+        star = CompanyStar.objects.get(author=author, company=company)
+        star.delete()
+
+        response_data['result'] = 'Company UnStar successful!'
+        response_data['post'] = company.pk
+
+        response_data['next'] = "Star"
+        response_data['next_url'] = "/companies/" + str(company.pk) + "/star/"
+
+        response_data['count'] = company.stars.count()
+        response_data['created'] = star.created_date.strftime('%B %d, %Y %I:%M %p')
+        response_data['author'] = star.author.username
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
