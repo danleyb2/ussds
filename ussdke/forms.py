518bed4df7142665e363907c883e7e6d54fce9ac
danleyb2 <ndieksman@gmail.com>
2016-12-29 07:52:40 +0300

    add register and login ability

--- ussdke/forms.py
+++ ussdke/forms.py
@@ -1,9 +1,26 @@
 from .models import USSD
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.admin import User
 
-from django.forms import ModelForm
-
-
-class USSDForm(ModelForm):
+class USSDForm(forms.ModelForm):
     class Meta:
         model=USSD
         fields= ['description',]
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(widget=forms.TextInput())
+    last_name = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
