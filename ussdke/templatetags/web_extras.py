b8d1dea80598c0d7684e7b24202bbe424113dc2b
danleyb2 <ndieksman@gmail.com>
2016-12-29 08:11:37 +0300

    fix TypeError, 'AnonymousUser' object is not iterable when checking stared companies

--- ussdke/templatetags/web_extras.py
+++ ussdke/templatetags/web_extras.py
@@ -6,5 +6,7 @@ register = template.Library()
 @register.filter(name='stared_by')
 def stared_by(company, user):
     # user_id = int(user.id) # get the user id
+    if not user.is_authenticated():
+        return 0
 
     return company.stargazer(user)
